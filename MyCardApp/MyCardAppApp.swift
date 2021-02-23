c11ea96ae8da228a3a57b1bf20d3b151529f0f76
userror <50100791+Userror101@users.noreply.github.com>
2021-02-23 16:21:18 +0500

    Initial Commit

---
+++ MyCardApp/MyCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardAppApp.swift
+//  MyCardApp
+//
+//  Created by userror on 23.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
